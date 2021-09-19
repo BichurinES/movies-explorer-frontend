95d26ac8e3bc25624f230248c50d5404e08238a1
Egor Bichurin <bichurin.es@gmail.com>
2021-09-19 14:30:01 +0500

    feat: реализована логика поиска фильмов

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import { searchMovies } from '../../utils/utils.js';
+import moviesApi from '../../utils/MoviesApi.js';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import './SearchForm.css';
 
 export default function SearchForm() {
+  const [search, setSearch] = useState('');
+
+  function changeSearchHandler(evt) {
+    setSearch(evt.target.value);
+  }
+
+  function submitHandler(evt) {
+    evt.preventDefault();
+    moviesApi.getData()
+      .then((data) => {
+        console.log(searchMovies(search, data))
+      })
+  }
+  
   return (
-    <form name="search-form" className="search-form">
+    <form name="search-form" className="search-form" onSubmit={ submitHandler }>
       <div className="search-form__search-bar">
-        <input className="search-form__field" type="text" name="movie-title" placeholder="Фильм" required />
+        <input className="search-form__field" type="text" name="search" placeholder="Фильм" required value={ search } onChange={ changeSearchHandler } />
         <input className="search-form__submit-button page__button" type="submit" />
       </div>
       <div className="search-form__filter">
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -20,3 +20,86 @@ export function formatCard(card) {
   delete formatedCard.updated_at;
   return formatedCard;
 }
+
+export function searchMovies(req, base) {
+  const normalizeReq = req.toLowerCase();
+  const result = [];
+  const exactMatch = [];
+  const oneOrMoreWordsMatch = [];
+  const descriptionMatch = [];
+
+  const keyWords = normalizeReq.split(' ').filter((word) => word.length > 1);
+  const oneKeyWord = keyWords.length === 1 ? keyWords[0] : null;
+  
+  function isFieldsIncludes(substr, firstField, secondField = null) {
+    return (firstField ? firstField.includes(substr) : false) || (secondField ? secondField.includes(substr) : false);
+  }
+
+  function getWordWithoutEnding(word) {
+    return word.length <= 4 ? word.slice(0, -1) : word.slice(0, -2);
+  }
+
+  function searchExactMatch(movie, normalizeData) {
+    const { nameRU, nameEN } = normalizeData;
+    if (
+      isFieldsIncludes(normalizeReq, nameRU, nameEN) || 
+      isFieldsIncludes(getWordWithoutEnding(oneKeyWord), nameRU, nameEN)
+      ) {
+      exactMatch.push(movie);
+      return;
+    }
+  }
+
+  function searchOneOrMoreWordsMatch(movie, normalizeData) {
+    if (!oneKeyWord) {
+      const { nameRU, nameEN, description } = normalizeData;
+
+      for (let i = 0; i < keyWords.length; i++) {
+        const shortWord = getWordWithoutEnding(keyWords[i]);
+        
+        if (
+          isFieldsIncludes(keyWords[i], nameRU, nameEN) || 
+          isFieldsIncludes(shortWord, nameRU, nameEN)
+        ) {
+          oneOrMoreWordsMatch.push(movie);
+          break;
+        }
+
+        if (
+          isFieldsIncludes(keyWords[i], description) || 
+          isFieldsIncludes(shortWord, description)
+        ) {
+          descriptionMatch.push(movie);
+          break;
+        }
+      }
+    }
+  }
+
+  function searchDescriptionMatch(movie, normalizeData) {
+    const { description } = normalizeData;
+    if (
+      isFieldsIncludes(normalizeReq, description) || 
+      isFieldsIncludes(getWordWithoutEnding(oneKeyWord), description)
+    ) {
+      descriptionMatch.push(movie);
+      return;
+    }
+  }
+
+  base.forEach((movie) => {
+    const { nameRU, nameEN, description } = movie;
+    const normalizeData = {
+      nameRU: nameRU ? nameRU.toLowerCase() : nameRU,
+      nameEN: nameEN ? nameEN.toLowerCase() : nameEN,
+      description: description ? description.toLowerCase() : description,
+    };
+    
+    searchExactMatch(movie, normalizeData);
+    searchOneOrMoreWordsMatch(movie, normalizeData);
+    searchDescriptionMatch(movie, normalizeData);
+  });
+
+  result.push(...exactMatch, ...oneOrMoreWordsMatch, ...descriptionMatch);
+  return result;
+}
